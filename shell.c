59f378a1d743dd20db50712f3a134cf2d7b0da0b
Nirhar <manishkausikhari@gmail.com>
2020-09-02 12:14:38 +0530

    parsing enabled

--- shell.c
+++ shell.c
@@ -4,8 +4,13 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 #include <string.h>
+#include <sys/types.h>
+#include <signal.h>
+
 
 #define BUFFER_SIZE 100
+#define MAX_NO_OF_ARGUMENTS 10
+#define MAX_LEN_OF_AN_ARGUMENT 20
 
 //General Purpose Buffer
 char buf[BUFFER_SIZE];
@@ -15,18 +20,19 @@ void showPrompt();
 char* getCmd();
 void storeHistory();
 void printHistory();
-int execCmd();
+char** parseCmd(char* cmd);
+int execCmd(char** args);
+int runOtherCmd(char** args);
+
 int main(){
     using_history();
     stifle_history(10);
     while(1){
         showPrompt();
         char* cmd=getCmd(); 
-        
         storeHistory(cmd);
-        execCmd(cmd);
-        
-        
+        char** args=parseCmd(cmd);
+        execCmd(args);
     }
     
 
@@ -53,27 +59,50 @@ void printHistory(){
         count++;
     }
 }
-int execCmd(char* cmd){
+char** parseCmd(char* cmd){
     int len=strlen(cmd);
     int ptr=0;
-    char x[BUFFER_SIZE]; 
-    while(cmd[ptr]!=' ' && ptr<len){x[ptr]=cmd[ptr];ptr++;}; 
-    x[ptr]='\0';
-    printf("%s\n",x);
-    if(!strcmp(x,"cd\0")){
-        char folder[BUFFER_SIZE];
-        int i=0;
+    int wptr=0;
+    int c=0;
+    char** args=(char**)malloc(MAX_NO_OF_ARGUMENTS*sizeof(char*));
+    while(ptr<len){
+        char* word=(char*)malloc(MAX_NO_OF_ARGUMENTS*sizeof(char));
+        while(cmd[ptr]!=' ' && ptr<len)word[wptr++]=cmd[ptr++];
+        word[ptr]='\0';
+        args[c++]=word;
+        wptr=0;
         ptr++;
-        while(ptr<len)folder[i++]=cmd[ptr++];
-        folder[i]='\0';
-        if(chdir(folder))printf("cd error: Directory not found\n");
-    }else if(!strcmp(x,"history")){
-        printHistory();
-    }else if(!strcmp(x,"exit")){
-        exit(EXIT_SUCCESS);
     }
+    return args;
+}
+int execCmd(char** args){
+    if(!strcmp(args[0],"cd")){                                                    //if cd is entered
+        if(chdir(args[1]))printf("cd error: Directory not found\n");
+    }else if(!strcmp(args[0],"history")){                                         //if history is entered
+        printHistory();
+    }else if(!strcmp(args[0],"exit")){                                            //if exit is entered
+        exit(EXIT_SUCCESS);
+    }else if(!strcmp(args[0],"kill")){                                            //if kill is entered
+        int pid=0;
+        int i=0;
+        while(args[1][i]!='\0')pid=pid*10+(args[1][i++]-'0');
+        printf("\n%d\n",pid);
+        if(!(kill(pid,SIGTERM)==0)){
+            printf("\nkill error: Either pid is invalid or you do not have permission to kill the process\n");
+            return 0;
+        }else{
+            printf("\nKilling Successful\n");
+        }
+        
+    }else if(!strcmp(args[0],"job")){                                             //if job is entered
 
+    }else{
+        runOtherCmd(args);
+    }
+   
+}
 
+int runOtherCmd(char** args){
     
 }
 /*
